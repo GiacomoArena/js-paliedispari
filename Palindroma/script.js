469065c1acc409d6fc2f065be89328f4ae6da12c
GiacomoArena <giacomoarena28@gmail.com>
2023-03-28 18:58:23 +0200

    update commit

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -4,7 +4,7 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
 */
-
+const body = document.querySelector('body');
 const parolaUtente = prompt("Scrivi una parola e ti diró se si tratta di una parola palindroma o no");
 const parolaMin = parolaUtente.toLowerCase();
 
@@ -16,13 +16,14 @@ const verificaPalindromo = palindroma(parolaMin);
 
 
 
+
 function palindroma(parolaUtente) {
   let parolaInversa = parolaUtente.split("").reverse().join("");
   if (parolaUtente === parolaInversa) {
-    console.log(parolaInversa + " é una parola palindroma");
+    body.append(parolaInversa + " é una parola palindroma");
   }
   else{
-    console.log(parolaInversa + " non é una parola palindroma");
+    body.append(parolaInversa + " non é una parola palindroma");
   }
   
 }
